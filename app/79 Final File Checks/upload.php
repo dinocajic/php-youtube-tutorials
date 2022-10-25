89b4173df3fdb2e70393ab21c98c43a5a50050a6
Dino Cajic <dinocajic@gmail.com>
2022-10-25 16:45:55 -0400

    Moved image size check below file extension check

--- app/79 Final File Checks/upload.php
+++ app/79 Final File Checks/upload.php
@@ -9,17 +9,17 @@ if ( file_exists($target_file) )
     die("The file already exists");
 }
 
-if ( getimagesize($temp_file) === false )
-{
-    die("You must upload an image");
-}
-
 $allowed   = ['jpg', 'png'];
 $extension = pathinfo($file_name, PATHINFO_EXTENSION);
 
 if ( ! in_array($extension, $allowed) )
 {
     die("The format is not correct. You may only upload: " . implode(", ", $allowed));
+}
+
+if ( getimagesize($temp_file) === false )
+{
+    die("You must upload an image");
 }
 
 $max_size_mb = 0.5;
