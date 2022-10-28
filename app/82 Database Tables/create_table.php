af90e10dbc5b83a5afe480168bd4f4bae7b20dba
Dino Cajic <dinocajic@gmail.com>
2022-10-28 16:48:40 -0400

    Create Table script for PHP

--- app/82 Database Tables/create_table.php
+++ app/82 Database Tables/create_table.php
@@ -2,19 +2,28 @@
 $server   = "localhost";
 $username = "admin";
 $password = "E9bIyRDwzidT";
+$database = "dino_test_db";
 
-$connection = new mysqli( $server, $username, $password );
+$connection = new mysqli( $server, $username, $password, $database );
 
 if ( $connection->connect_error ) {
     die( $connection->connect_error );
 }
 
-$sql = "CREATE DATABASE dino_test_db_3";
+$sql = "CREATE TABLE users (
+            id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            first_name VARCHAR(255),
+            last_name VARCHAR(255),
+            email VARCHAR(255),
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+                       ON UPDATE CURRENT_TIMESTAMP,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)";
 
-if ( $connection->query($sql) === TRUE ) {
-    echo "Database created successfully";
+if ($connection->query($sql) === TRUE) {
+    echo "Table users created successfully";
 } else {
-    die($connection->error);
+    echo $connection->error;
 }
 
 echo "Connection established successfully";
