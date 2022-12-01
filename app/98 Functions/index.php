3b303896da1608f853cc3394af5243b9aff688c5
Dino Cajic <dinocajic@gmail.com>
2022-11-30 19:40:36 -0500

    String functions

--- app/98 Functions/index.php
+++ app/98 Functions/index.php
@@ -19,3 +19,73 @@ var_dump($string);
 $html = '<script>alert("Hacked");</script>';
 $sanitized = htmlspecialchars($html);
 var_dump($sanitized);
+
+// AddSlashes
+var_dump( addslashes("Hello there. What's up?") );
+var_dump( addslashes('She said, "I am not interested."') );
+var_dump( addslashes('c:\users\dino"') );
+
+// str_replace
+var_dump( str_replace("Hello", "Hey", "Hello, what's going on?") );
+
+$search = ["Hello", "Hey", "Hi"];
+var_dump( str_replace($search, "Good Morning", "Hey There. How are you? I said Hello.") );
+
+$search  = ["Hello", "Hey", "Hi"];
+$replace = ["Good Morning", "Good Day", "Good Evening"];
+var_dump( str_replace($search, $replace, "Hey There. How are you? I said Hello.") );
+
+// str_len
+var_dump( strlen("Dino") );
+
+// strtolower
+var_dump( strtolower("Dino") );
+var_dump( strtolower("DINO") );
+
+// strtoupper
+var_dump( strtoupper("Dino") );
+var_dump( strtoupper("dino") );
+
+// ucfirst
+var_dump( ucfirst("hey there. how's it going?") );
+
+// strpos
+var_dump( strpos("Hi there.", "Hi") );
+var_dump( strpos("Hi there.", "there") );
+var_dump( strpos("Hi there.", "ere") );
+var_dump( strpos("Hi there.", "frank") );
+
+if ( strpos("Hi there", "Hi") != false ) {
+    echo "Found a match";
+} else {
+    echo "Didn't find a match";
+}
+
+if ( strpos("Hi there", "Hi") !== false ) {
+    echo "Found a match";
+} else {
+    echo "Didn't find a match";
+}
+
+var_dump( strpos("Hi there.", "hi") );
+var_dump( stripos("Hi there.", "hi") );
+
+var_dump( strrpos("Hi there. Hi.", "Hi") );
+
+var_dump( strrpos("Hi there. Hi.", "hi") );
+var_dump( strripos("Hi there. Hi.", "hi") );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
